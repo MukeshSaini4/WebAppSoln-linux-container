c355a4ca29c4b31f5109e619412a9de541ab16a3
MukeshSaini4 <sainiclasses.pune@gmail.com>
2026-05-25 15:51:07 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,34 +2,16 @@
 
 echo "===== START DEPLOYMENT ====="
 
-# Go to app directory
 cd /home/ubuntu/app
 
-# Fix permissions
-chmod +x scripts/*.sh
-
-# Clean old files
-echo "Cleaning old files..."
-rm -rf /home/ubuntu/app/*
-
-# Copy fresh files from deployment archive
-echo "Copying fresh files..."
-cp -r /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/* /home/ubuntu/app/
-
-# Move to app folder again
-cd /home/ubuntu/app
-
-# Stop old container
 echo "Stopping old container..."
 docker stop myapp || true
 docker rm myapp || true
 
-# Build Docker image
-echo "Building Docker image..."
-docker build -t myapp .
+echo "Building image..."
+docker build -t myapp -f WebApp/Dockerfile .
 
-# Run container
 echo "Running container..."
 docker run -d -p 8080:8080 --name myapp myapp
 
-echo "===== DEPLOYMENT COMPLETE ====="
+echo "===== DONE ====="
